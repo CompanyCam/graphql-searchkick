7c2ceaaf4ba00dc4c7adce1b54703d831ba82ce5
Chad Wilken <chad.wilken@gmail.com>
2020-03-17 16:11:15 -0500

    Initial commit

---
+++ lib/graphql/searchkick.rb
@@ -0,0 +1,8 @@
+require "graphql/searchkick/version"
+
+module Graphql
+  module Searchkick
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/graphql/searchkick/version.rb
@@ -0,0 +1,5 @@
+module Graphql
+  module Searchkick
+    VERSION = "0.1.0"
+  end
+end
